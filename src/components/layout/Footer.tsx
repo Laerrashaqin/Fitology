4d99922447b57bf8389b5c745e500f9fd68ea20e
Laerrashaqin <laerrashaqinzahna@gmail.com>
2026-07-12 00:36:20 +0700

    add layout footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer({ lang }: Props) {
     <footer className="bg-black text-white py-12">
       <div className="max-w-7xl mx-auto px-6 lg:px-8 flex flex-col md:flex-row justify-between items-center gap-6">
         <div className="text-xl font-black tracking-tighter uppercase">
-          FITOLOGY.
+          FITOLOGY<span className="text-orange-500">.</span>
         </div>
         <div className="text-[10px] text-slate-400 font-bold tracking-widest uppercase">
           &copy; 2026 Fitology •{" "}
